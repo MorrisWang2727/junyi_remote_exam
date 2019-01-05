f1a668ac4ab967193dd74b2186d060ab4ab290ff
kate3018250 <kate3018250@gmail.com>
2019-01-05 14:09:29 +0800

    Question1 solutions

---
+++ q1_solution.py
@@ -0,0 +1,29 @@
+# Q1_A
+def get_reversed_word(input_word):
+    if type(input_word) == str:
+        return input_word[::-1]
+
+# Q1_B
+def get_reversed_sentence(input_sentence):
+    word_list = input_sentence.split()
+    reversed_word_list = list()
+
+    for word in word_list:
+        reversed_word_list.append(get_reversed_word(word))
+
+    reversed_sentence = ' '.join(reversed_word_list)
+    
+    return reversed_sentence
+
+if __name__ == "__main__":
+    # Q1_A
+    test_str = "junyiacademy"
+    ans_str = get_reversed_word(test_str)
+    # Expected True.
+    print(ans_str == "ymedacaiynuj")
+
+    # Q1_B
+    test_sentence = "flipped class room is important"
+    ans_sentence = get_reversed_sentence(test_sentence)
+    # Expected True.
+    print(ans_sentence == "deppilf ssalc moor si tnatropmi")
